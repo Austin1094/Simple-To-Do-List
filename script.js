17adbcf35c7bb260dec3e616ba27698e3a171bc3
Austin Adams <Austin.Adams1094@gmail.com>
2023-11-29 12:22:43 -0500

    Centered items and created function to display text for to do list

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTodo() {
+    const todo = document.getElementById('new-todo').value;
+    if (todo.length < 3) {
+        return alert("You must have at least 3 characters for your to do item")
+    }
+
+    const list = document.getElementById("list");
+    const li = document.createElement("li");
+    const text = document.createTextNode(todo);
+    li.appendChild(text);
+
+    list.appendChild(li);
+}
